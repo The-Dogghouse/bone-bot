e11b844a835ec4534211ff3f4b7281c50201e098
Evan Black <evan@evan.black>
2025-10-17 11:56:05 -0400

    Sus images sent as DM's, forwards, @mentions, and replies

--- bone-bot.py
+++ bone-bot.py
@@ -10,6 +10,7 @@ from tempfile import TemporaryDirectory
 import subprocess
 from shutil import which
 from pathlib import Path
+import io
 
 BOT_VERSION = "0.2.0"
 
@@ -32,6 +33,112 @@ rusty_sussy_path = None
 async def on_ready():
     await tree.sync()
     logger.info(f"Bot logged in as {client.user}")
+
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+
+    if client.user not in message.mentions and not isinstance(
+        message.channel, discord.DMChannel
+    ):
+        return
+
+    if client.user in message.mentions:
+        logger.info(f"Bot @mentioned by {message.author}")
+
+        if len(message.attachments) > 0:
+            logger.info(f"@mention message has attachment {message.attachments[0]}")
+            attachment = message.attachments[0]
+        elif (
+            message.reference is None
+            or message.reference.resolved is None
+            or len(message.reference.resolved.attachments) == 0
+        ):
+            logger.info(f"Reply-ed message {message} has no attachments, 'woof'-ing")
+            await message.reply("Woof")
+            return
+        else:
+            attachment = message.reference.resolved.attachments[0]
+
+    else:
+        logger.info(f"Bot direct messaged by {message.author}")
+
+        if (
+            len(message.message_snapshots) > 0
+            and len(message.message_snapshots[0].attachments) > 0
+        ):
+            logger.info(f"DM message {message} is a forward with an attachment")
+            attachment = message.message_snapshots[0].attachments[0]
+        elif len(message.attachments) > 0:
+            logger.info(f"DM message {message} has an attachment")
+            attachment = message.attachments[0]
+        else:
+            logger.info(
+                f"DM message {message} has no attachments and is not a forward with attachments, 'woof'-ing"
+            )
+            await message.reply("Woof")
+            return
+
+    if not attachment.content_type or not attachment.content_type.startswith("image/"):
+        logger.info(
+            f"Attachment`{attachment}` from message `{message}` content type was not image, got `{attachment.content_type}`, ignoring"
+        )
+
+        # Yes, we're really getting this petty
+        vowels = ["a", "e", "i", "o", "u"]
+        await message.reply(
+            f"That doesn't smell like an image. It smells like a{'n' if attachment.content_type[0].lower() in vowels else ''} `{attachment.content_type}`"
+        )
+        return
+
+    logger.info(f"Message {message} has an image attachment, sussing")
+    sus_buffer = await sus_image(attachment, 21)
+
+    if sus_buffer is None:
+        logger.error(f"Failed to sus `{attachment}` from `{message}`, bailing out")
+        return
+
+    logger.info(f"Sussed `{attachment}` replying to `{message}`")
+    await message.reply(file=discord.File(sus_buffer, filename="sussed.gif"))
+
+
+async def sus_image(image: discord.Attachment, width: int) -> Optional[io.BytesIO]:
+    with TemporaryDirectory() as tmp_dir:
+        input_path = tmp_dir + "/" + image.filename
+        output_path = tmp_dir + "/output.gif"
+
+        logger.info(f"Saving input image `{image.filename}` to `{input_path}`")
+
+        image_bytes = await image.read()
+        with open(input_path, "wb") as f:
+            f.write(image_bytes)
+
+        logger.info(f"Input image saved to `{input_path}`, running `rusty-sussy`")
+
+        sussy_result = subprocess.run(
+            [
+                rusty_sussy_path,
+                f"--input={input_path}",
+                f"--output={output_path}",
+                f"--width={width}",
+            ],
+            capture_output=True,
+        )
+
+        if sussy_result.returncode != 0:
+            logger.error(
+                f"Failed running 'bone-sus' with "
+                "`image`: `{image}`, "
+                "`width`: `{width}`, "
+                "`stderr`: `{sussy_result.stderr}`, "
+                "`stdout`: `{sussy_result.stdout}`"
+            )
+            return None
+
+        with open(output_path, "rb") as f:
+            return io.BytesIO(f.read())
 
 
 @tree.command(name="bone-teams", description="Generate teams from a channel")
@@ -123,46 +230,18 @@ async def bone_sus(
         )
         return
 
-    with TemporaryDirectory() as tmp_dir:
-        input_path = tmp_dir + "/" + image.filename
-        output_path = tmp_dir + "/output.gif"
+    result_buffer = await sus_image(image, width)
 
-        logger.info(f"Saving input image `{image.filename}` to `{input_path}`")
+    if result_buffer is None:
+        await interaction.followup.send(f"Error sussing image!", ephemeral=True)
+        return
 
-        image_bytes = await image.read()
-        with open(input_path, "wb") as f:
-            f.write(image_bytes)
+    logger.info(
+        f"Successfully sussed `{image}`, replying to {interaction.user.display_name}"
+    )
 
-        logger.info(f"Input image saved to `{input_path}`, running `rusty-sussy`")
-
-        sussy_result = subprocess.run(
-            [
-                rusty_sussy_path,
-                f"--input={input_path}",
-                f"--output={output_path}",
-                f"--width={width}",
-            ],
-            capture_output=True,
-        )
-
-        if sussy_result.returncode != 0:
-            logger.error(
-                f"Failed running 'bone-sus' for {interaction.user.display_name} with "
-                "`image`: `{image}`, "
-                "`width`: `{width}`, "
-                "`stderr`: `{sussy_result.stderr}`, "
-                "`stdout`: `{sussy_result.stdout}`"
-            )
-            await interaction.followup.send(f"Error sussing image!", ephemeral=True)
-            return
-
-        logger.info(
-            f"Successfully sussed `{input_path}`, into `{output_path}`, replying to {interaction.user.display_name}"
-        )
-
-        with open(output_path, "rb") as f:
-            discord_file = discord.File(f, filename="sussed.gif")
-            await interaction.followup.send(file=discord_file)
+    discord_file = discord.File(result_buffer, filename="sussed.gif")
+    await interaction.followup.send(file=discord_file)
 
 
 def read_file(file: str) -> List[str]:
